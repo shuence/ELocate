a2e16499c053dbc108396a54cdcbd67447199fb6
shuence <Shubhampitekar98@gmail.com>
2023-11-07 21:55:44 +0530

    Starting The Project

---
+++ src/app/Components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Home
---
+++ src/app/Header/Navbar.tsx
@@ -0,0 +1,117 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { IonIcon } from "@ionic/react";
+import { menuOutline } from "ionicons/icons";
+import { closeOutline } from "ionicons/icons";
+import { location } from "ionicons/icons"
+import logo from "../../assets/ELocate-s.png"
+
+interface NavItemProps {
+  label: string;
+}
+
+const Header = () => {
+  const [isNavbarActive, setIsNavbarActive] = useState(false);
+  const [isHeaderActive, setIsHeaderActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsHeaderActive(true);
+      } else {
+        setIsHeaderActive(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleNavbar = () => {
+    setIsNavbarActive(!isNavbarActive);
+  };
+
+  return (
+    <header className={`header ${isHeaderActive ? "active" : ""}`} data-header>
+      <div className="container">
+      <Link href="/">
+          <Image
+            src={logo}
+            alt="ELocate"
+            width={100}
+            height={100}
+            className="logo ml-4 logo md:ml-16 "
+            />
+        </Link>
+
+        <nav className={`navbar ${isNavbarActive ? "active" : ""}`} data-navbar>
+          <div className="wrapper">
+            <Link href="/" className="logo">
+              ELocate
+            </Link>
+            <button
+              className="nav-close-btn"
+              aria-label="close menu"
+              data-nav-toggler
+              onClick={toggleNavbar}
+            >
+              <IonIcon
+                icon={closeOutline}
+                className={`close-icon ${isNavbarActive ? "" : "hidden"}`}
+              ></IonIcon>
+            </button>
+          </div>
+
+          <ul className="navbar-list">
+            <NavItem label="Home" />
+            <NavItem label="About" />
+            <NavItem label="Facilities" />
+            <NavItem label="Education" />
+            <NavItem label="Contact" />
+          </ul>
+        </nav>
+
+        <h1 className=' font-montserrat font-bold text-emerald-600 flex items-center gap-[1vh]'>
+        <IonIcon icon={location} aria-hidden="true" role="img"></IonIcon>
+Location</h1>
+
+        <Link href="/login" className="btn-outline md:mr-8">
+          Login
+        </Link>
+
+        <button
+          className="nav-open-btn"
+          aria-label="open menu"
+          data-nav-toggler
+          onClick={toggleNavbar}
+        >
+          <IonIcon icon={menuOutline} aria-hidden="true" role="img"></IonIcon>
+        </button>
+
+        <div
+          className={`overlay ${isNavbarActive ? "active" : ""}`}
+          data-nav-toggler
+          data-overlay
+          onClick={toggleNavbar}
+        ></div>
+      </div>
+    </header>
+  );
+};
+
+const NavItem = ({ label }: NavItemProps) => {
+  return (
+    <li className="navbar-link">
+      <Link href={label === "Home" ? "/" : `/${label.toLowerCase()}`}>
+        {label}
+      </Link>
+    </li>
+  );
+};
+
+export default Header;
