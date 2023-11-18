9d8e98f43fd035cb62a2ff13d2e80cb70a31bdbb
Shubham Pitekar <65482186+shuence@users.noreply.github.com>
2023-11-18 23:59:25 +0530

    Update EFacility.tsx

--- src/app/e-facilities/EFacility.tsx
+++ src/app/e-facilities/EFacility.tsx
@@ -52,12 +52,14 @@ const FacilityMap = () => {
           .then((response) => response.json())
           .then((data) => {
             const address = data.features[0]?.place_name || "Address not available";
-const uniqueWords = [...new Set(address.split(', '))];
-const newAddress = uniqueWords.join(', ');
+const uniqueWordsSet = new Set(address.split(', '));
+const uniqueWordsArray = Array.from(uniqueWordsSet);
+const newAddress = uniqueWordsArray.join(', ');
 
+// Set state and then log
+setAddresses((prevAddresses) => [...prevAddresses, newAddress]);
 console.log(address);
-            setAddresses((prevAddresses) => [...prevAddresses, newAddress]);
-            console.log(newAddress)
+console.log(newAddress);
             const popup = new mapboxgl.Popup().setHTML(
               `<h3>${facility.name}</h3>
               <p>Capacity: ${facility.capacity}</p>
