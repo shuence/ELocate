cceeb1fe127ddd162e95bfb620aa6930aafb3e20
shuence <Shubhampitekar98@gmail.com>
2023-11-19 20:25:54 +0530

    Small Bug Fix

---
+++ src/app/e-facilities/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import FacilityMap from "./Efacility";
+
+const Page = () => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -20 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="my-8 py-8">
+          <FacilityMap />
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Page;
